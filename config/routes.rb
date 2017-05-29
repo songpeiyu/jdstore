e51fd4aac11be4d96daa4fa73f26e887ea83cfb5
宋沛雨 <speiy191625@gmail.com>
2017-05-29 08:40:03 +0800

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
      resources :products
+     resources :orders
  end
  resources :products do
        member do
@@ -24,7 +25,7 @@ Rails.application.routes.draw do
         post :pay_with_wechat
       end
     end
-  
+
   namespace :account do
    resources :orders
  end
